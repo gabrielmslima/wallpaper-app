d9b22c3eef78c1c3ff2537d3c4fd433269eaf1ac
gabrielmslima <gabrielnunesinfor@gmail.com>
2024-07-22 19:14:25 -0300

    Update build_files.sh script for better build process

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,13 @@
-pip install -r requirements.txt 
-python3.9 manage.py collectstatic
+echo " BUILD START"
+
+python3.9 --version
+python3.9 -m ensurepip # não aparece nos tutoriais internacionais
+python3.9 -m pip install -r requirements.txt
+
+echo " MAKE MIGRATIONS..."
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
+
+
+python3.9 manage.py collectstatic --noinput --clear
+echo " BUILD END"
