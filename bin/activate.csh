5ca5c66a92ea6679180768ab2c6106ca277c3654
gabrielmslima <gabrielnunesinfor@gmail.com>
2024-07-21 22:55:28 -0300

    Layout finished and images are showing correctly

--- bin/activate.csh
+++ bin/activate.csh
@@ -9,7 +9,7 @@ alias deactivate 'test $?_OLD_VIRTUAL_PATH != 0 && setenv PATH "$_OLD_VIRTUAL_PA
 # Unset irrelevant variables.
 deactivate nondestructive
 
-setenv VIRTUAL_ENV "/home/gabriel/dev/wallpapers-gallery"
+setenv VIRTUAL_ENV "/home/gabriel/dev/CLIs/wallpaper-app"
 
 set _OLD_VIRTUAL_PATH="$PATH"
 setenv PATH "$VIRTUAL_ENV/bin:$PATH"
@@ -18,8 +18,8 @@ setenv PATH "$VIRTUAL_ENV/bin:$PATH"
 set _OLD_VIRTUAL_PROMPT="$prompt"
 
 if (! "$?VIRTUAL_ENV_DISABLE_PROMPT") then
-    set prompt = "(wallpapers-gallery) $prompt"
-    setenv VIRTUAL_ENV_PROMPT "(wallpapers-gallery) "
+    set prompt = "(wallpaper-app) $prompt"
+    setenv VIRTUAL_ENV_PROMPT "(wallpaper-app) "
 endif
 
 alias pydoc python -m pydoc
--- myproject/wallpapers_gallery/views.py
+++ myproject/wallpapers_gallery/views.py
@@ -4,11 +4,13 @@ from django.shortcuts import render
 def fetch_images_from_github(repo_url):
     response = requests.get(repo_url)
     if response.status_code == 200:
-        return response.json()
+        images_data = response.json()
+        images_info = [{'name': img['name'], 'download_url': img['download_url']} for img in images_data]
+        return images_info
     return []
 
 def wallpapers_view(request):
     repo_url = "https://api.github.com/repos/gabrielmslima/wallpapers/contents/images"
-    images = fetch_images_from_github(repo_url)
-    context = {'images': images}
+    images_info = fetch_images_from_github(repo_url)
+    context = {'images_info': images_info}
     return render(request, 'wallpapers_gallery/gallery.html', context)
